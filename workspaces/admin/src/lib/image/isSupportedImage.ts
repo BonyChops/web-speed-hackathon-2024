4cd43b0af452e240af23035bac06804f293624cc
BonyChops <contact@bonychops.com>
2024-03-24 02:50:13 +0900

    fileType 修正

--- workspaces/admin/src/lib/image/isSupportedImage.ts
+++ workspaces/admin/src/lib/image/isSupportedImage.ts
@@ -1,10 +1,10 @@
-import fileType from 'file-type';
+import {fileTypeFromBuffer} from 'file-type';
 
 const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif', 'image/jxl'];
 
 export async function isSupportedImage(image: File): Promise<boolean> {
   const arrayBuffer = await image.arrayBuffer();
-  const type = await fileType.fileTypeFromBuffer(arrayBuffer);
+  const type = await fileTypeFromBuffer(arrayBuffer);
   if (type === undefined) {
     return false;
   }
