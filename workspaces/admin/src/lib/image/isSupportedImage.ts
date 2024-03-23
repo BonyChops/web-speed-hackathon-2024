7bb5ea17632237568d8078f3c0dee825680647a9
BonyChops <contact@bonychops.com>
2024-03-24 02:48:23 +0900

    ファイル判定ロジック変更

--- workspaces/admin/src/lib/image/isSupportedImage.ts
+++ workspaces/admin/src/lib/image/isSupportedImage.ts
@@ -1,28 +1,12 @@
-import { fileTypeFromBuffer } from 'file-type';
-import { Magika } from 'magika';
+import fileType from 'file-type';
 
-const SUPPORTED_MAGIKA_LABEL_LIST = ['bmp', 'jpeg', 'png', 'webp'];
 const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif', 'image/jxl'];
 
-const magika = new Magika();
-
-const initMagikaPromise = magika.load({
-  configURL: '/assets/magika/config.json',
-  modelURL: '/assets/magika/model.json',
-});
-
 export async function isSupportedImage(image: File): Promise<boolean> {
-  await initMagikaPromise;
-  const prediction = await magika.identifyBytes(new Uint8Array(await image.arrayBuffer()));
-
-  if (SUPPORTED_MAGIKA_LABEL_LIST.includes(prediction.label)) {
-    return true;
+  const arrayBuffer = await image.arrayBuffer();
+  const type = await fileType.fileTypeFromBuffer(arrayBuffer);
+  if (type === undefined) {
+    return false;
   }
-
-  const fileType = await fileTypeFromBuffer(await image.arrayBuffer());
-  if (SUPPORTED_MIME_TYPE_LIST.includes(fileType?.mime ?? '')) {
-    return true;
-  }
-
-  return false;
+  return SUPPORTED_MIME_TYPE_LIST.includes(type?.mime);
 }
